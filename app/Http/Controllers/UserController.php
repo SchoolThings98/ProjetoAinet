17508551d25e41ef5d36ca95a00eb6de218a1d43
SchoolThings98 <2181854@my.ipleiria.pt>
2020-05-20 11:25:38 +0100

    ja ta a dara para gravar as alteraçoes da permisssoes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
-use App\Http\Request\UserPost;
+use App\Http\Requests\UserPost;
 
 class UserController extends Controller
 {
--- app/Http/Requests/UserPost.php
+++ app/Http/Requests/UserPost.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+
 use Illuminate\Foundation\Http\FormRequest;
+
 
 class UserPost extends FormRequest
 {
