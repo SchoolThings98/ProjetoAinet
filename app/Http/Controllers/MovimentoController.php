0dfa1f91af96f382e9f89799145a902d10f56fc0
ZiZiC4 <2181836@my.ipleiria.pt>
2020-05-25 17:07:32 +0100

    alteração MovimentoController

--- app/Http/Controllers/MovimentoController.php
+++ app/Http/Controllers/MovimentoController.php
@@ -16,37 +16,7 @@ class MovimentoController extends Controller
 	public function index(Request $request)
 	{
 
-
         $qry = Movimento::where('id','>=','0');
-        /*if($request->has('name')){
-            $qry->where('name','like','%'.$request->query('name').'%');
-        }
-        if($request->has('email')){
-            $qry->where('email',$request->query('email'));
-        }*/
-
-        /*if (request()->query('tipo')) {
-            $movimentos = $movimentos->where('tipo', '=', 'D');
-        }
-        if (request()->query('tipo')) {
-            $movimentos = $movimentos->where('tipo', '=','R');
-        }
-        if(request()->query('confirmado') == '1'){
-            $movimentos = $movimentos->where('confirmado', '=', '1');
-        }
-        if(request()->query('confirmado') == '0'){
-            $movimentos = $movimentos->where('confirmado', '=', '0');
-        }*/
-
-
-        /* if($request->has('nome')){
-            $categoria = Categoria::where('nome', $request->query('nome'));
-            // $qry->where('categoria_id',$categoria->id());
-        } */
-        /* if($request->has('tipo','>=','D')){
-            $qry->where('tipo','like','%'.$request->query('tipo').'%');
-        } */
-
         $conta_id = $request->conta_id;
         $qry = Movimento::query();
         $qry->where('conta_id',$conta_id);
