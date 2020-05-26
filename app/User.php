e0d1e784381896b3d5cb01c6a72acb772f90ba66
SchoolThings98 <2181854@my.ipleiria.pt>
2020-05-26 19:58:34 +0100

    little detail

--- app/User.php
+++ app/User.php
@@ -38,17 +38,6 @@ class User extends Authenticatable implements MustVerifyEmail
         'email_verified_at' => 'datetime',
     ];
 
-    /*
-    public function userNormal(){
-
-        return $this->hasOne('App\UserNormal');
-    }
-
-    public function admin(){
-
-        return $this->hasOne('App\Admin');
-    }
-    */
 
     public function contas(){
         return $this->belongsToMany('App\Conta','autorizacoes_contas','user_id','conta_id');
